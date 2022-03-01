35bbcfac2dd2f1a7a2d6806a9154f46e35945fc4
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2022-02-28 23:34:59 -0500

    Update LoginController.java

--- mvc_calidad_project/src/main/java/controlador/LoginController.java
+++ mvc_calidad_project/src/main/java/controlador/LoginController.java
@@ -15,41 +15,53 @@ import modelo.entidades.Usuario;
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String STR_USUARIO = "usuario";
+	private static final String STR_LOGIN_PAGE = "/jsp/login.jsp";
        
     public LoginController() {
         super();
     }
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/jsp/login.jsp").forward(request, response);
+		try {
+			request.getRequestDispatcher(STR_LOGIN_PAGE).forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String usuario = request.getParameter("usuario");
-		String password = request.getParameter("password");
-		Usuario usuarioAutenticado = UsuarioDAO.autenticar(usuario, password);
-		if(usuarioAutenticado != null) {
-			HttpSession session = request.getSession(true);
-			switch(usuarioAutenticado.getTipoUsuario()) {
-			case "U":
-				session.setAttribute("usuario",usuarioAutenticado); 
-				session.setAttribute("usuarioID",usuarioAutenticado.getIdUsuario());
-				response.sendRedirect("UListarTicketController");
-				break;
-			case "D":
-				session.setAttribute("usuario",usuarioAutenticado);
-				response.sendRedirect("DListarTicketController");
-				break;
-			default:
-				session.invalidate(); 
-				request.getRequestDispatcher("/jsp/login.jsp").forward(request, response);
-				break;
-			
+		try {
+			String usuario = request.getParameter(STR_USUARIO);
+			String password = request.getParameter("password");
+			Usuario usuarioAutenticado = UsuarioDAO.autenticar(usuario, password);
+			if(usuarioAutenticado != null) {
+				HttpSession session = request.getSession(true);
+				switch(usuarioAutenticado.getTipoUsuario()) {
+				case "U":
+					session.setAttribute(STR_USUARIO,usuarioAutenticado); 
+					session.setAttribute("usuarioID",usuarioAutenticado.getIdUsuario());
+					response.sendRedirect("UListarTicketController");
+					break;
+				case "D":
+					session.setAttribute(STR_USUARIO,usuarioAutenticado);
+					response.sendRedirect("DListarTicketController");
+					break;
+				default:
+					session.invalidate(); 
+					request.getRequestDispatcher(STR_LOGIN_PAGE).forward(request, response);
+					break;
+				
+				}
 			}
-		}
-		else {
-			request.getRequestDispatcher("/jsp/login.jsp").forward(request, response);
+			else {
+				request.getRequestDispatcher("/jsp/login.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
